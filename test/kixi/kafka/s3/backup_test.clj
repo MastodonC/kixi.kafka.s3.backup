b15fc1632f23db3e4c0730028e54ec751a37d718
Jason Bell <jason@mastodonc.com>
2017-06-12 10:30:44 +0100

    Remove unused test.

--- test/kixi/kafka/s3/backup_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kixi.kafka.s3.backup-test
-  (:require [clojure.test :refer :all]
-            [kixi.kafka.s3.backup :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
